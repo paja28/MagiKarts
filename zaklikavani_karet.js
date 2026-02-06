fbdcba3a1c7ded13e2e1570854d86fd43334e180
paja28 <dvorakp.07@spst.eu>
2026-02-06 19:57:33 +0100

    udělaný objekty a další detaily, hráč může útočit, protihráč ještě ne

--- zaklikavani_karet.js
+++ zaklikavani_karet.js
@@ -3,15 +3,17 @@
     var hrac_id_chybejicich_karet = ["karta1","karta2","karta3","karta4","karta5"];
     var protihrac_id_chybejicich_karet = ["protihrac_karta1","protihrac_karta2","protihrac_karta3","protihrac_karta4","protihrac_karta5"];
     var protihrac_prostredek_prazdne_misto = ["protihrac_pole_karta1","protihrac_pole_karta2","protihrac_pole_karta3","protihrac_pole_karta4"];
-    var protihrac_inventar_objekty_karty =[];
+    var protihrac_prostredek_prazdne_misto_chybejici =[];
+    var protihrac_inventar_objekty_karty =[];       
     var protihrac_prostredek_objekty_karty = [];
-    var protihrac_utocne_karty = []; //
     var pole_karet = [Lucistnik,Bojovnik,Carodej,Spartan,Fireball];
     var hrac_inventar_objekty_karty = [];     //v tomhle jsou objekty, která má jakákoliv karta v hráčovím inventáři
     var hrac_prostredek_objekty_karty = [];
     var index_hrac_karty_objekty = 0;
+    var utocici_karta_objekt = null;
+    var byvala_zakliknuta_karta_id ="";//Slouží aby při rychlém kliknutí na kartu v inventáři, se neprobugovala nahoru
+    var vysouvani_pomocna_promenna2 =0;//Slouží aby při rychlém kliknutí na kartu v inventáři, se neprobugovala nahoru
 
-    var zkouska=0;
     window.onload = function() {    //dá hráči prvních 5 random karet
         for(let i =0;i<5;i++){
             pridani_karty("hrac");
@@ -24,7 +26,8 @@
         let prazdna_mista;
         //karta.classList.toggle("zakliknuta_karta");       
         //karta.classList.toggle("karty");              Nefunguje, nevím proč
-        
+
+
         if(!je_zakliknuta_karta)
         {
             je_zakliknuta_karta=true;
@@ -35,6 +38,7 @@
             prazdna_mista = document.querySelectorAll(".prazdne_misto");
             if(prazdna_mista.length>0){
             for(let i =0;i<prazdna_mista.length;i++){
+                prazdna_mista[i].classList.add("clickable");
                 prazdna_mista[i].setAttribute("onclick","presunuti_karty(\'"+prazdna_mista[i].id+"\')");
             }
         }
@@ -43,13 +47,19 @@
             if(zakliknuta_karta_id==id)  {  //kontrola jestli hráč kliknul na již vysunutou kartu
                 je_zakliknuta_karta=false;                       
                 karta.classList.remove("zakliknuta_karta");
-                karta.classList.add("vysouvani_karet");                //Přidat sem, aby se classa vysouvani_karet přidala o vteřinu cca později, bude to mít lepší efekt
+                byvala_zakliknuta_karta_id = zakliknuta_karta_id;
                 zakliknuta_karta_id ="";
-
                 prazdna_mista = document.querySelectorAll(".prazdne_misto");
                 for(let i =0;i<prazdna_mista.length;i++){
+                    prazdna_mista[i].classList.remove("clickable");
                 prazdna_mista[i].removeAttribute("onclick");            
-            }
+                }
+                setTimeout(function(){
+                    console.log(byvala_zakliknuta_karta_id,zakliknuta_karta_id);
+                    if(byvala_zakliknuta_karta_id!=zakliknuta_karta_id)
+                    karta.classList.add("vysouvani_karet");
+                },200)
+            
             }
             else
             {
@@ -58,15 +68,15 @@
                 document.getElementById(zakliknuta_karta_id).classList.remove("zakliknuta_karta");
                 karta.classList.add("zakliknuta_karta");
                 karta.classList.remove("vysouvani_karet");
+
+                byvala_zakliknuta_karta_id = zakliknuta_karta_id;
                 zakliknuta_karta_id = id;
 
-
                 prazdna_mista = document.querySelectorAll(".prazdne_misto");
-                if(prazdna_mista.length>0){
                 for(let i =0;i<prazdna_mista.length;i++){
+                prazdna_mista[i].classList.add("clickable");
                 prazdna_mista[i].setAttribute("onclick","presunuti_karty(\'"+prazdna_mista[i].id+"\')");
                 }
-            }
             }
         }
             
@@ -89,6 +99,7 @@
             hrac_id_chybejicich_karet.push(zakliknuta_karta_id);//dává do pole id karty, která hráčovi chybí
             if(!document.getElementById("pridavani_karet").hasAttribute("onclick")) //přidání attributu, aby si hráč mohl nakliknout další kartu, když jich nemá 5
             {
+                document.getElementById("pridavani_karet").classList.add("clickable");
                 document.getElementById("pridavani_karet").setAttribute("onclick","pridani_karty(\"hrac\")");
             }
 
@@ -98,15 +109,19 @@
             presunuta_karta.id=id_prazdneho_mista;
             presunuta_karta.setAttribute("onclick","utok("+presunuta_karta.id+")");
             //
+                
+            document.getElementById("pridavani_karet").classList.add("clickable");
 
             hracovo_pole_vykladani.append(presunuta_karta);
             je_zakliknuta_karta=false;
             zakliknuta_karta_id=null;
 
+            for(let i =0;i<prazdna_mista.length;i++){               //nastavení všech prázdných míst, aby nebyli klikatelný
+                prazdna_mista[i].classList.remove("clickable");    
+            }
+
             //tady začíná hrát protihráč
             protihrac_vybrani_random_karty();
-
-
         }
         else
             console.log("Chyba, zakliknuta_karta je rovna null. Ve funkci presunuti_karty()");
@@ -141,7 +156,7 @@
        
         nova_karta_img.setAttribute("id", id_chybejici_karty);
         if(hrac_nebo_protihrac=="hrac"){
-        nova_karta_img.classList.add("karty", "vysouvani_karet");
+        nova_karta_img.classList.add("karty", "vysouvani_karet","clickable");
         nova_karta_img.setAttribute("onclick", "nakliknuto('" + id_chybejici_karty + "')");
         }
     else
@@ -151,7 +166,8 @@
        
         karty_hrace_nebo_protihrace.appendChild(nova_karta_img);  
     } else {
-       document.getElementById("pridavani_karet").removeAttribute("onclick");
+        document.getElementById("pridavani_karet").classList.remove("clickable");
+        document.getElementById("pridavani_karet").removeAttribute("onclick");
     }    
     }
 
@@ -170,14 +186,13 @@
 
     function protihrac_presunuti_karty(){
         const protihrac_prazdne_misto = protihrac_prostredek_prazdne_misto.splice(0,1);
+        protihrac_prostredek_prazdne_misto_chybejici.push(protihrac_prazdne_misto);
         const karta_na_vymazani = document.querySelector(".vysouvani_karet_protihrace");
-        console.log(karta_na_vymazani);
         for(let i =0;i<protihrac_inventar_objekty_karty.length;i++){
             if(karta_na_vymazani.id==protihrac_inventar_objekty_karty[i].id)
             {
                 protihrac_prostredek_objekty_karty.push(protihrac_inventar_objekty_karty[i]);
                 protihrac_inventar_objekty_karty.splice(i,1);
-                console.log("presunuto");
                 break;
             }
         }
@@ -187,46 +202,41 @@
         document.getElementById(protihrac_prazdne_misto).remove();//vymazání prázdného místa na středu
         protihrac_presunuta_karta.classList.remove("vysouvani_karet_protihrace");
         protihracovo_pole_vykladani_karet.append(protihrac_presunuta_karta);
-        protihrac_utocne_karty.push(protihrac_presunuta_karta.id);
         console.log(protihrac_prostredek_objekty_karty,protihrac_inventar_objekty_karty);
     }
 
     function utok(karta_utocna){
-        let utocna_karta_objekt=null;
-        console.log(hrac_prostredek_objekty_karty);
         for(let i = 0;i<hrac_prostredek_objekty_karty.length;i++){
                 if(hrac_prostredek_objekty_karty[i].id==karta_utocna.id){
-                    utocna_karta_objekt = hrac_prostredek_objekty_karty[i];      //předělání id u objektu při přesunu
+                    utocici_karta_objekt = hrac_prostredek_objekty_karty[i];      //předělání id u objektu při přesunu
                     break;
                 }
             }
-            console.log(utocna_karta_objekt);
-        for(let i =0; i < protihrac_utocne_karty.length;i++){
-            document.getElementById(protihrac_utocne_karty[i]).setAttribute("onclick","snizeni_hp(\""+protihrac_utocne_karty[i]+"\", "+utocna_karta_objekt.id+")");
+            console.log(utocici_karta_objekt);
+        for(let i =0; i < protihrac_prostredek_objekty_karty.length;i++){
+            console.log(document.getElementById(protihrac_prostredek_objekty_karty[i].id));
+            document.getElementById(protihrac_prostredek_objekty_karty[i].id).setAttribute("onclick","snizeni_hp(\""+protihrac_prostredek_objekty_karty[i].id+"\")");
         }
     }
 
     function snizeni_hp(karta_snizeni_hp_id,utocna_karta_element){
         let index_utocne_karty =0;
         let index_poskozene_karty =0;
-        for(let i =0;i<protihrac_prostredek_objekty_karty.length;i++){  //nalezení hráčového objektu
-            if(protihrac_prostredek_objekty_karty[i].id=karta_snizeni_hp_id)
+        for(let i =0;i<protihrac_prostredek_objekty_karty.length;i++){  //nalezení protihracoveho objektu
+            if(protihrac_prostredek_objekty_karty[i].id==karta_snizeni_hp_id)
             {
                 index_poskozene_karty = i;
             }
         }
-        for(let i =0;i<hrac_prostredek_objekty_karty.length;i++){  //nalezení hráčového objektu
-            if(hrac_prostredek_objekty_karty[i].id=utocna_karta_element.id)
-            {
-                index_utocne_karty = i;
-            }
-        }
-        protihrac_prostredek_objekty_karty[index_poskozene_karty].hp -= hrac_prostredek_objekty_karty[index_utocne_karty].dmg;
+        protihrac_prostredek_objekty_karty[index_poskozene_karty].hp -= utocici_karta_objekt.dmg;
         if(protihrac_prostredek_objekty_karty[index_poskozene_karty].hp<=0){
+            const prazdne_misto_prostredek = document.createElement("div");
+            prazdne_misto_prostredek.id = protihrac_prostredek_prazdne_misto_chybejici.shift();
+            prazdne_misto_prostredek.classList.add("protivnik_prazdne_misto");
+            document.getElementById("pole_vykladani_protihrace").appendChild(prazdne_misto_prostredek).innerHTML="Prazdné_místo";
+            console.log(document.getElementById("pole_vykladani_protihrace"));
             document.getElementById(protihrac_prostredek_objekty_karty[index_poskozene_karty].id).remove();
             protihrac_prostredek_objekty_karty.splice(index_poskozene_karty,1);
         }
-            //Udělat vymazání karty
-        console.log(protihrac_prostredek_objekty_karty); 
     }
 
